5d3884438c3d04166770edd250fee7ef37efbfb8
Mokoolaid <55265990+Mokoolaid@users.noreply.github.com>
2020-02-01 14:58:40 -0800

    Added arg processes
    added arg proceses

--- keys.js
+++ keys.js
@@ -2,9 +2,8 @@ console.log('this is loaded');
 
 exports.spotify = {
 
-    N
+
     id: process.env.SPOTIFY_ID,
-    secret: ProcessingInstruction.env.SPOTIFY_SECRET
+    secret: process.env.SPOTIFY_SECRET
 };
-OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=2292c881
-2292c881
+//OMDb API: http:www.omdbapi.com/?i=tt3896198&apikey=2292c881
--- liri.js
+++ liri.js
@@ -6,19 +6,36 @@ var axios = require("axios");
 var Spotify = require("node-spotify-api");
 
 var spotifyKeyInfo = require("./keys.js");
+var bandName
+var songName
+var type = process.argv[2]
+
+if (type === "song") {
+    songName = process.argv[3]
+}
+else if (type === "band") {
+    bandName = process.argv[3]
+}
+else {
+    bandName = process.argv[3]
+    songName = process.argv[3]
+
+}
+console.log(bandName);
+console.log(songName);
 
 //concert this uses the Bands in Town Artist Events API. An axios.get sent the search request and the results were console.logged using moment to change the format of the returned date.
 var queryURL = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=[key]";
-    
-    console.log(queryURL); 
 
-    axios.get(queryURL).then(
-        function(bandResponse){
-            console.log("Venue: " + bandResponse.data[0].venue.name);
-            console.log("City: " + bandResponse.data[0].venue.city);
-            console.log(moment(bandResponse.data[0].datetime).format("MM/DD/YYYY"));
-        }
-    );
+console.log(queryURL);
+
+axios.get(queryURL).then(
+    function (bandResponse) {
+        console.log("Venue: " + bandResponse.data[0].venue.name);
+        console.log("City: " + bandResponse.data[0].venue.city);
+        console.log(moment(bandResponse.data[0].datetime).format("MM/DD/YYYY"));
+    }
+);
 
 //Spotify this:
 var spotify = new Spotify({
@@ -26,10 +43,11 @@ var spotify = new Spotify({
     secret: spotifyKeyInfo["spotify"].secret
 });
 
-spotify.request('https://api.spotify.com/v1/search?q=track:' + songName + '&type=track&limit=10', function(error, songResponse) {
-    if (error){
+spotify.request('https://api.spotify.com/v1/search?q=track:' + songName + '&type=track&limit=10', function (error, songResponse) {
+    if (error) {
         return console.log(error);
     }
+
     console.log("Artist: " + songResponse.tracks.items[0].artists[0].name);
     console.log("Song: " + songResponse.tracks.items[0].name);
     console.log("URL: " + songResponse.tracks.items[0].preview_url);
@@ -39,31 +57,31 @@ spotify.request('https://api.spotify.com/v1/search?q=track:' + songName + '&type
 
 var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=[key]";
 
-    axios.get(queryURL).then(
-        function(movieResponse){
-            console.log("Title: " + movieResponse.data.Title);
-            console.log("Year: " + movieResponse.data.Year);
-            console.log("Rated: " + movieResponse.data.imdbRating);
-            console.log("Country: " + movieResponse.data.Country);
-            console.log("Language: " + movieResponse.data.Language);
-            console.log("Plot: " + movieResponse.data.Plot);
-            console.log("Actors: " + movieResponse.data.Actors);
-            console.log("Rotten Tomatoes: " + movieResponse.data.Ratings[1].Value);
-        }
-    );
+axios.get(queryURL).then(
+    function (movieResponse) {
+        console.log("Title: " + movieResponse.data.Title);
+        console.log("Year: " + movieResponse.data.Year);
+        console.log("Rated: " + movieResponse.data.imdbRating);
+        console.log("Country: " + movieResponse.data.Country);
+        console.log("Language: " + movieResponse.data.Language);
+        console.log("Plot: " + movieResponse.data.Plot);
+        console.log("Actors: " + movieResponse.data.Actors);
+        console.log("Rotten Tomatoes: " + movieResponse.data.Ratings[1].Value);
+    }
+);
 //Do what it says:
 
 function doWhatInfo() {
 
-    fs.readFile("random.txt", "utf8", function(error, data) {
-      if (error) {
-        return console.log(error);
-      }
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
         var output = data.split(",");
         for (var i = 0; i < output.length; i++) {
             console.log(output[i]);
         }
-      });
+    });
 };
 
 
